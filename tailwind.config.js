545975241164dc1c18f5b5c05f88c66ed05cda82
Omeir <omeirf.02@gmail.com>
2022-12-31 22:54:51 +0530

    add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ['Poppins', 'sans-serif'],
+        Valofont: ['ValorantFont', 'monospace'],
+      },
+
   },
   plugins: [],
 }
