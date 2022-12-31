2c0a048b3ed84fb485a62f6ea28711f3956dd57f
Omeir <omeirf.02@gmail.com>
2022-12-31 22:56:19 +0530

    new component: SelectDropdown

---
+++ components/SelectDropdown.tsx
@@ -0,0 +1,37 @@
+// @ts-nocheck
+
+import { useContext } from 'react';
+import { agentIds } from '../data/agentIds';
+import { StoreContext, StoreDispatch } from './Store';
+
+export const SelectDropdown = () => {
+  const globalStore = useContext(StoreContext);
+  const setGlobalStore = useContext(StoreDispatch);
+
+  const handleChange = (e: any) => {
+    console.log(e.target.value);
+    setGlobalStore({
+      ...globalStore,
+      selectedAgent: e.target.value,
+    });
+  };
+  return (
+    <div className='flex justify-center'>
+      <select
+        className='w-full p-2 text-base text-gray-700 transition ease-in-out bg-white bg-no-repeat border border-gray-300 border-solid rounded-md font-primary bg-clip-padding focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
+        aria-label='Default select example'
+        onChange={handleChange}
+        title='Choose an agent'
+      >
+        {agentIds.map((agent) => (
+          <option
+            value={agent.uuid}
+            selected={globalStore.selectedAgent === agent.uuid}
+          >
+            {agent.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
